7dbd99e4ced10886f181f906d1468eea493fe28f
moshfiqrony <moshfiqrony@gmail.com>
2021-07-14 12:26:49 +0600

    MR | First commit

---
+++ black_order/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BlackOrderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'black_order'
---
+++ chitauri/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ChitauriConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'chitauri'
