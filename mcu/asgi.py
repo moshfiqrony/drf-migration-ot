5afa5f01e6b8862a9380f941329aa8cfe3350d0d
moshfiqrony <moshfiqrony@gmail.com>
2021-07-14 12:23:39 +0600

    MR | Updated

--- mcu/asgi.py
+++ mcu/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mig_ot project.
+ASGI config for mcu project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mig_ot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mcu.settings')
 
 application = get_asgi_application()
